0415dddc5145e8c7d029ec0b3e119091c4ada1c5
Imran Khan <imrankhan@stevemadden.com>
2024-04-18 11:04:40 +0530

    Unit Test project

---
+++ CurrencyConversion.UnitTests/ConversionServiceTest.cs
@@ -0,0 +1,31 @@
+using CurrencyConversion.Services.Exceptions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Services;
+using System.Threading;
+
+namespace CurrencyConversion.UnitTests
+{
+    public class ConversionServiceTest
+    {
+
+        [Fact]
+        public void GetCurrencyRates_Success()
+        {
+            string sourecCurrency = "";
+            string targetCurrency = "INR";
+            decimal amount = 100;
+
+            var ihostingMoq = new Mock<Microsoft.AspNetCore.Hosting.IHostingEnvironment>();
+
+            var conversionService = new ConversionService(ihostingMoq.Object);
+
+            Assert.Throws<BadRequestException>(() => conversionService.GetCurrencyRates(sourecCurrency, targetCurrency, amount));
+
+
+        }
+
+
+    }
+}
